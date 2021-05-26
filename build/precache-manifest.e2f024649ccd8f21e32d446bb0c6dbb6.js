8bb4a2dc0ccadcdb0361d09e664aec54b67b809f
Yifan Wu <94yifanwu@gmail.com>
2021-05-26 05:12:37 -0700

    finish frontend

---
+++ build/precache-manifest.e2f024649ccd8f21e32d446bb0c6dbb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9416b11b779a7a855379ffdeb423ff0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b71d03dab16a17e6669",
+    "url": "/static/css/main.ca03ca9e.chunk.css"
+  },
+  {
+    "revision": "71a855231aad6abc21f2",
+    "url": "/static/js/2.59596ec3.chunk.js"
+  },
+  {
+    "revision": "698c7d1ab7fc994ffbdcf6ff99e91ff0",
+    "url": "/static/js/2.59596ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b71d03dab16a17e6669",
+    "url": "/static/js/main.80c654e9.chunk.js"
+  },
+  {
+    "revision": "723b8de4144d7b4aea6d",
+    "url": "/static/js/runtime-main.562f75cf.js"
+  }
+]);
