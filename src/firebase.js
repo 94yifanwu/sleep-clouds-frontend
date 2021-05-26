71aaace682fb84f46dc8ec2a4a710775dbc23597
Yifan Wu <94yifanwu@gmail.com>
2021-05-26 04:28:46 -0700

    update

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAO4elqQgp47g8O6Pv9pOVN9klZ0wFLD2Y",
+    authDomain: "twitter-clone-723d4.firebaseapp.com",
+    projectId: "twitter-clone-723d4",
+    storageBucket: "twitter-clone-723d4.appspot.com",
+    messagingSenderId: "595004510393",
+    appId: "1:595004510393:web:bc48af160148251ea88b68",
+    measurementId: "G-ZQPY6PWMCM"
+  };
