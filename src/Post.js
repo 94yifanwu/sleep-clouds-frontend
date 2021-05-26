db18176343e3087ae5259786ff774a96c1fb6a77
Yifan Wu <94yifanwu@gmail.com>
2021-05-26 02:46:09 -0700

    init

---
+++ src/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Post.css"
+function Post({
+    displayName,
+    username,
+    varified,
+    timestamp
+}) {
+    return (
+        <div className="post">
+            
+        </div>
+    )
+}
+
+export default Post
